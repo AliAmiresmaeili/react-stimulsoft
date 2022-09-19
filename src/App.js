e2e123358c9391fc314be1aa3f26ef3df96ce3e8
Ali Amiresmaeili <aliamiresmaeili@gmail.com>
2022-09-19 09:12:13 +0430

    stimulsoft package

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect } from "react";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  useEffect(() => {
+    console.log("Loading Viewer view");
+
+    console.log("Creating the report viewer with default options");
+    var viewer = new window.Stimulsoft.Viewer.StiViewer(
+      null,
+      "StiViewer",
+      false
+    );
+
+    console.log("Creating a new report instance");
+    var report = new window.Stimulsoft.Report.StiReport();
+
+    console.log("Load report from url");
+    report.loadFile("/reports/SimpleList.mrt");
+
+    console.log(
+      "Assigning report to the viewer, the report will be built automatically after rendering the viewer"
+    );
+    viewer.report = report;
+
+    console.log("Rendering the viewer to selected element");
+    viewer.renderHtml("viewer");
+  }, []);
+
+  return <div id="viewer"></div>;
 }
 
 export default App;
