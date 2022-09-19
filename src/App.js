1b0d8c18737e532e4347483bfe04bf419168bc57
Ali Amiresmaeili <aliamiresmaeili@gmail.com>
2022-09-19 10:09:19 +0430

    add styles & options for persian

--- src/App.js
+++ src/App.js
@@ -1,16 +1,34 @@
 import { useEffect } from "react";
-import "./App.css";
 
 function App() {
   useEffect(() => {
+    window.Stimulsoft.Base.Localization.StiLocalization.addLocalizationFile(
+      "/stimulSoft/fa.xml",
+      false,
+      "fa"
+    );
+    window.Stimulsoft.Base.Localization.StiLocalization.cultureName = "fa";
+
+    var options = new window.Stimulsoft.Viewer.StiViewerOptions();
+    options.appearance.scrollbarsMode = false;
+    options.appearance.rightToLeft = true;
+    options.appearance.fullScreenMode = false;
+    options.appearance.scrollbarsMode = false;
+    options.toolbar.viewMode =
+      window.Stimulsoft.Viewer.StiWebViewMode.SinglePage;
+    options.toolbar.zoom = 90;
+    options.toolbar.showSaveButton = true;
+    options.toolbar.showAboutButton = false;
+    //options.toolbar.fontFamily = "IRANSans_faNum";
+
     var viewer = new window.Stimulsoft.Viewer.StiViewer(
-      null,
+      options,
       "StiViewer",
       false
     );
 
     var report = new window.Stimulsoft.Report.StiReport();
-    report.loadFile("/reports/ReportJS.mrt");
+    report.loadFile("/stimulsoft/reports/ReportJS.mrt");
     viewer.report = report;
     viewer.renderHtml("viewer");
   }, []);
